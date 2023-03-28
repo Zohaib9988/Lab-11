e67a4d06aa841a0437e9cd30095907a2c635357a
Zohaib Alvi <129130948+Zohaib9988@users.noreply.github.com>
2023-03-28 02:00:28 +0000

    Added a new print statement with some math.

--- .ccls-cache/@home@runner@Lab-11-CS-211-Mar-28-2023/main.c
+++ .ccls-cache/@home@runner@Lab-11-CS-211-Mar-28-2023/main.c
@@ -2,6 +2,11 @@
 
 int main(void) {
   printf("Hello World\n");
-  printf("Hello world 222\n");
+  
+  int x = 1;
+  int y = 2;
+  int z = x + y;
+
+  printf("The result of x and y is %d", z);
   return 0;
 }
